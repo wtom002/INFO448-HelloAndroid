df4cddefca7e8297eea6dd73960544ed7ec85004
William Tom <wtom002@uw.edu>
2023-10-04 21:47:14 -0700

    hw1 changes

---
+++ HelloAndroid/myapplication/src/main/java/edu/uw/ischool/wtom002/myapplication/MainActivity.kt
@@ -0,0 +1,18 @@
+package edu.uw.ischool.wtom002.myapplication
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity :
+    AppCompatActivity() {
+    override fun onCreate(
+        savedInstanceState: Bundle?
+    ) {
+        super.onCreate(
+            savedInstanceState
+        )
+        setContentView(
+            R.layout.activity_main
+        )
+    }
+}
